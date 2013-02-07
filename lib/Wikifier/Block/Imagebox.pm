09b1c0dea5b33592a137d6848247d4631b5c3e2f
Mitchell Cooper <cooper879@msn.com>
2013-02-07 15:22:11 -0500

    temporarily hardcoded to notroll.net image generator.

--- lib/Wikifier/Block/Imagebox.pm
+++ lib/Wikifier/Block/Imagebox.pm
@@ -53,14 +53,31 @@ sub parse {
 # HTML.
 sub result {
     my $block  = shift;
+<<<<<<< HEAD
 
     # parse formatting in the image description.
     my $description = $block->wikifier->parse_formatted_text($block->{description});
 
+=======
+    
+    # TODO: do not hardcode to notroll.net!
+    my $height = $block->{height}; my $width = $block->{width};
+    
+    # image generator does not accept units.
+    $height =~ s/px//; $width =~ s/px//;
+    
+    # if automatic scaling is desired, omit the width and height options.
+    $height = '' if $height eq 'auto'; $width = '' if $width eq 'auto';
+    
+    my $fullurl  =
+    my $shorturl = 'http://images.notroll.net/paranoia/files/'.$block->{file};
+    $shorturl   .= "?height=$height&width=$width&cropratio=";
+    
+>>>>>>> temporarily hardcoded to notroll.net image generator.
     return <<END;
 <div class="wiki-imagebox wiki-imagebox-$$block{align}">
-    <a href="full url">
-        <img src="short url" alt="image" style="width: $$block{width}; height: $$block{height};" />
+    <a href="$fullurl">
+        <img src="$shorturl" alt="image" style="width: $$block{width}; height: $$block{height};" />
     </a>
     <div class="wiki-imagebox-description">$description</div>
 </div>
