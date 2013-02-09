21a726b446db8d5849c9f79cf6bc8001da2532a1
Mitchell Cooper <cooper879@msn.com>
2013-02-09 15:58:19 -0500

    way too small

--- lib/Wikifier.pm
+++ lib/Wikifier.pm
@@ -532,7 +532,7 @@ sub parse_format_type {
         when ('ref') {
             $page->{reference_number} ||= 1;
             my $ref = $page->{reference_number}++;
-            return "<sup style=\"font-size: 50%\"><a href=\"#\">[$ref]</a></sup>";
+            return "<sup style=\"font-size: 80%\"><a href=\"#\">[$ref]</a></sup>";
         }
     
     }
