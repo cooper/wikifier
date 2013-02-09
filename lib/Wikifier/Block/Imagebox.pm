5c75a15baa6314f9c2c679637c26043a9a5af250
Mitchell Cooper <cooper879@msn.com>
2013-02-09 14:05:46 -0500

    apply width to inner div.

--- lib/Wikifier/Block/Imagebox.pm
+++ lib/Wikifier/Block/Imagebox.pm
@@ -142,8 +142,8 @@ sub result {
     }
     
     return <<END;
-<div class="wiki-imagebox wiki-imagebox-$$block{align}" style="width: $w;">
-    <div class="wiki-imagebox-inner">
+<div class="wiki-imagebox wiki-imagebox-$$block{align}">
+    <div class="wiki-imagebox-inner" style="width: $w;">
     <a href="$link_address">
         <img src="$image_url" alt="image" style="width: $w; height: $h;"$js />
     </a>
