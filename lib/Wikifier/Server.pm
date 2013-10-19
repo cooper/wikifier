0b489f1e25feda7ea0b805111b27367ad94105cd
Mitchell Cooper <cooper879@msn.com>
2013-10-18 22:51:46 -0400

    more work to server; creation of Connection class

---
+++ lib/Wikifier/Server.pm
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+# Copyright (c) 2013 Mitchell Cooper
+package Wikifier::Server;
+
+use warnings;
+use strict;
+use feature 'say';
+
+use IO::Async::Loop;
+use IO::Async::Listener;
+use IO::Socket::UNIX;
+use JSON qw(encode_json decode_json);
+
+use Wikifier::Wiki;
+use Wikifier::Server::Connection;
+
+# start the server.
+sub start {
+    my $conf = shift;
+    
+    # create a loop.
+    my $loop = IO::Async::Loop->new;
+
+    # create a new listener and add it to the loop.
+    my $listener = IO::Async::Listener->new(on_stream => \&handle_stream);
+    $loop->add($listener);
+
+    # if a file already exists and is a socket, I assume we should delete it.
+    my $path = $conf->get('server.socket.path');
+    if ($path && -S $path) {
+        unlink $path;
+    }
+
+    # create the socket.
+    my $socket = IO::Socket::UNIX->new(
+        Local  => $path || die("No socket file path specified\n"),
+        Listen => 1
+    ) or die "Can't create UNIX socket: $!\n";
+
+    # begin listening.
+    $listener->listen(handle => $socket);
+    say 'Listening on '.$path;
+
+    # run forever.
+    $loop->run;
+    
+}
+
+# handle a new stream.
+sub handle_stream {
+    my (undef, $stream) = @_;
+   
+    say "New connection $stream";
+    $stream->{connection} = Wikifier::Server::Connection->new($stream);
+   
+    # configure the stream.
+    $stream->configure(
+        on_read         => \&handle_data,
+        on_write_eof    => sub { shift->{connection}->close },
+        on_read_eof     => sub { shift->{connection}->close },
+        on_read_error   => sub { shift->{connection}->close },
+        on_write_error  => sub { shift->{connection}->close }
+    );
+    
+    # add the stream to the loop.
+    $loop->add($stream);
+
+}
+
+# handle incoming data.
+sub handle_data {
+    my ($stream, $buffref, $eof) = @_;
+    while ($$buffref =~ s/^(.*?)\n//) {
+        # handle the data ($1)
+        $stream->{connection}->handle($1);
+    }
+}
---
+++ lib/Wikifier/Server/Connection.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# Copyright (c) 2013 Mitchell Cooper
+package Wikifier::Server::Connection;
+
+use warnings;
+use strict;
+
+use JSON qw(encode_json decode_json);
+
+# create a new connection.
+sub new {
+    my ($class, $stream) = @_;
+    return bless { stream => $stream }, $class;
+}
+
+# write a line of JSON-encoded data.
+sub send {
+    my ($connection, @etc) = @_;
+    $connection->{stream}->write(encode_json(\@etc)."\n");
+}
+
+# close the connection.
+sub close {
+    my $connection = shift;
+    my $stream = delete $connection->{stream};
+    delete $stream->{connection};
+    $stream->close;
+}
+
+# handle a line of data.
+sub handle {
+    my ($connection, $line) = @_;
+    
+    # make sure it's a JSON array.
+    my $data = eval { decode_json($line) };
+    if (!$line || !$data) {
+        $connection->send(error => { reason => 'Message must be a JSON array' });
+        $connection->close;
+        return;
+    }
+    
+}
