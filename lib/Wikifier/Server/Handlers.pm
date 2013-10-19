17d312177788167c185fd4c47edd836a2a617bee
Mitchell Cooper <cooper879@msn.com>
2013-10-19 14:16:53 -0400

    password authentication

--- lib/Wikifier/Server/Handlers.pm
+++ lib/Wikifier/Server/Handlers.pm
@@ -6,11 +6,27 @@ use warnings;
 use strict;
 use feature qw(say switch);
 
+use Digest::SHA1 'sha1_hex';
+
 my ($loop, $conf) = ($Wikifier::server::loop, $Wikifier::Server::conf);
 
 sub handle_wiki {
     my ($connection, $msg) = _required(@_, qw(name password)) or return;
-
+    my $name = (split /\./, $msg->{name})[0];
+    
+    # ensure that this wiki is configured on this server.
+    if (!$conf->get("server.wiki.$name")) {
+        $connection->error("Wiki '$$msg{name}' not configured on this server");
+        return;
+    }
+    
+    # see if the passwords match.
+    my $encrypted = sha1_hex($msg->{password});
+    if ($encrypted ne $conf->get("server.wiki.$name.password")) {
+        $connection->error("Passwords do not match");
+        return;
+    }
+    
     say "Got wiki command: $$msg{name} $$msg{password}";
 }
 
@@ -20,7 +36,7 @@ sub _required {
     my ($connection, $msg, @required) = @_;
     foreach (@required) {
         next if defined $msg->{$_};
-        $connection->error('Required option \''.$_.'\' missing');
+        $connection->error("Required option '$_' missing");
         return;
     }
     return my @a = ($connection, $msg);
