929e9fdd25634c815477edeaee38281274e6bff3
Mitchell Cooper <cooper879@msn.com>
2014-01-18 14:24:59 -0500

    paragraph-clear

--- lib/Wikifier/Block/Paragraph.pm
+++ lib/Wikifier/Block/Paragraph.pm
@@ -10,19 +10,24 @@ use strict;
 
 our %block_types = (
     paragraph => {
-        html  => \&paragraph_html,
+        html  => sub { paragraph_html(0, @_) },
         alias => 'p'
+    },
+    'paragraph-clear' => {
+        html  => sub { paragraph_html(1, @_) },
+        alias => 'p-clear'
     }
 );
 
 sub paragraph_html {
-    my ($block, $page) = @_;
+    my ($clear, $block, $page) = @_;
 
     # Parse formatting.
     my $html = Wikifier::Utilities::indent($block->{content}[0]);
-    $html = $page->parse_formatted_text($html);
-
-    return "<p class=\"wiki-paragraph\">\n$html\n</p>\n";
+    $html    = $page->parse_formatted_text($html);
+    $clear   = $clear ? 'clear ' : '';
+    
+    return "<p class=\"${clear}wiki-paragraph\">\n$html\n</p>\n";
 }
 
 __PACKAGE__
