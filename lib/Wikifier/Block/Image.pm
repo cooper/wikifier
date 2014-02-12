7dad1a681e83a7ca077981e4e061ac973043d399
Mitchell Cooper <cooper879@msn.com>
2014-02-11 21:02:58 -0500

    due to extremely similar code, image and imagebox have been merged.

--- lib/Wikifier/Block/Image.pm
+++ lib/Wikifier/Block/Image.pm
@@ -5,37 +5,55 @@
 # this is rarely used inside of a section. most images are displayed using imageboxes
 # instead. however, some hash-based blocks such as infoboxes make use of plain old images.
 #
+# imageboxes display a linked image previews with a caption.
+#
 package Wikifier::Block::Image;
 
 use warnings;
 use strict;
 
 use Carp;
+use HTML::Entities qw(encode_entities);
 
 our %block_types = (
     image => {
         base  => 'hash',
         parse => \&image_parse,
-        html  => \&image_html
+        html  => sub { image_html(0, @_) }
+    },
+    imagebox => {
+        base  => 'hash',
+        parse => \&image_parse,
+        html  => sub { image_html(1, @_) }
     }
 );
 
 # Hash handles the actual parsing; this assigns
-# properties to the image from the found values.
+# properties to the imagebox from the found values.
 sub image_parse {
-    my $block = shift;
-    $block->parse_base(@_);
+    my ($block, $page) = (shift, @_);
+    $block->parse_base(@_); # call hash parse.
     
-    $block->{$_} = $block->{hash}{$_} foreach qw(file width height);
+    $block->{$_} = $block->{hash}{$_} foreach qw(
+        description file width height
+        align float author license
+    );
     
     # no width or height specified; default to 100 width.
     if (!$block->{width} && !$block->{height}) {
         $block->{width} = 100;
     }
+    else {
+        $block->{width}  =~ s/px// if defined $block->{width};
+        $block->{height} =~ s/px// if defined $block->{height};
+    }
     
     # default to auto.
     $block->{width}  ||= 'auto';
     $block->{height} ||= 'auto';
+    
+    # no float; default to right.
+    $block->{float} ||= $block->{align} || 'right';
     
     # no file - this is mandatory.
     if (!length $block->{file}) {
@@ -48,33 +66,61 @@ sub image_parse {
 
 # HTML.
 sub image_html {
-    my ($block, $page) = (shift, @_);
+    my ($box, $block, $page, $el) = @_;
+    my ($w, $h, $link_address, $image_url, $image_root, $javascript, $height, $width);
     
-    # currently only exact pixel sizes or 'auto' are supported.
-    my $px_h = $block->{height}; my $px_w = $block->{width};
-    my ($w, $h) = ($px_w, $px_h);
-    $px_h  =~ s/px// if defined $px_h;
-    $px_w  =~ s/px// if defined $px_w;
+    ($height, $width) = ($block->{height}, $block->{width});
+    $image_root       = $page->wiki_info('image_root');
+    $link_address     = $image_url = "$image_root/$$block{file}";
     
-    my ($link_address, $image_url) = q();
-    my $image_root = $page->wiki_info('image_root');
+    $el->add_class('imagebox-'.$block->{float}) if $box;
     
-    # direct link to image.
-    $link_address = $image_url = "$image_root/$$block{file}";
+    ##############
+    ### SIZING ###
+    ##############
     
-    # we just won't do anything special.
-    if (lc $page->wiki_info('size_images') eq 'javascript') {
+    # decide which dimension(s) were given.
+    my $given_width  = defined $width  && $width  ne 'auto' ? 1 : 0;
+    my $given_height = defined $height && $height ne 'auto' ? 1 : 0;
+    
+    # both dimensions were given, so we need to do no sizing.
+    if ($given_width && $given_height) {
+        $w =  $width.q(px);
+        $h = $height.q(px);
+    }
+    
+    # use javascript image sizing
+    # - uses full-size images directly and uses javascript to size imageboxes.
+    # - this voids the validity as XHTML 1.0 Strict.
+    # - causes slight flash on page load (when images are scaled.)
+    elsif (lc $page->wiki_info('size_images') eq 'javascript') {
+    
+        # inject javascript resizer if no width is given.
+        if (!$given_width) {
+            $javascript = 1;
+        }
+        
+        # use the image root address options to determine URL.
+        
+        # width and height dummies will be overriden by JavaScript.
+        $w = $given_width  ? $width  : '200px';
+        $h = $given_height ? $height : 'auto';
+        
         $image_url = "$image_root/$$block{file}";
     }
     
     # use server-side image sizing.
+    # - maintains XHTML 1.0 Strict validity.
+    # - eliminates flash on page load.
+    # - faster (since image files are smaller.)
+    # - require read access to local image directory.
     elsif (lc $page->wiki_info('size_images') eq 'server') {
     
         # find the resized dimensions.
         ($w, $h) = $page->wiki_info('image_calc')->(
             file   => $block->{file},
-            height => $px_h,
-            width  => $px_w,
+            height => $height,
+            width  => $width,
             page   => $page
         );
         
@@ -85,14 +131,69 @@ sub image_html {
             width  => $w,
             page   => $page
         );
-
+    
         $image_url = $url;
+        ($w, $h)   = ($w.q(px), $h.q(px));
     }
-    return <<END;
-<div class="wiki-image">
-    <a href="$link_address"><img style="height: ${h}px; width: ${w}px;" src="$image_url" alt="image" /></a>
-</div>
-END
+    
+    ############
+    ### HTML ###
+    ############
+    
+    # this is not an image box; it's just an image.
+    if (!$box) {
+        my $a = $el->create_child(
+            type       => 'a',
+            attributes => { href => $link_address }
+        );
+        $a->create_child(
+            type       => 'img',
+            attributes => { src => $image_url },
+            alt        => 'image',
+            styles     => {
+                width  => $w,
+                height => $h
+            }
+        );
+        return;
+    }
+    
+    # create inner box with width restriction.
+    my $inner = $el->create_child(
+        class  => 'imagebox-inner',
+        styles => { width => $w }
+    );
+    
+    # create the anchor.
+    my $a = $inner->create_child(
+        type       => 'a',
+        attributes => { href => $link_address }
+    );
+    
+    # create the image.
+    my $img = $a->create_child(
+        type       => 'img',
+        attributes => { src => $image_url },
+        alt        => 'image',
+        styles     => {
+            width  => $w,
+            height => $h
+        }
+    );
+    
+    # insert javascript if using browser sizing. 
+    $img->add_attribute(onload =>
+        q{this.parentElement.parentElement.style.width = }.
+        q{this.offsetWidth + 'px'; this.style.width = '100%';}
+    ) if $javascript;
+    
+    # description.
+    my $desc = $inner->create_child(class => 'imagebox-description');
+    $desc->create_child(
+        class   => 'imagebox-description-inner',
+        content => encode_entities($page->parse_formatted_text($block->{description}))
+    );
+
 }
 
 __PACKAGE__
