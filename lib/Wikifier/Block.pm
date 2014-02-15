40d086e18fac9ad36ee67299b99cd549094becc7
Mitchell Cooper <cooper879@msn.com>
2014-02-15 17:33:29 -0500

    debug

--- lib/Wikifier/Block.pm
+++ lib/Wikifier/Block.pm
@@ -90,6 +90,7 @@ sub _parse {
         my $type_opts = $Wikifier::BlockManager::block_types{$type};
         if ($type_opts->{parse} && !$block->{parse_done}{$type}) {
             $type_opts->{parse}->($block, @_);
+            print "calling {parse} on $$block{type}: $block, (@_)\n";
             $block->{parse_done}{$type} = 1;
         }
         $type = $type_opts->{base};
