cfb517661b95f913f0a98a10a1123382c28ef734
Mitchell Cooper <cooper879@msn.com>
2014-02-15 18:06:03 -0500

    revert

--- lib/Wikifier/Block/Section.pm
+++ lib/Wikifier/Block/Section.pm
@@ -57,11 +57,8 @@ sub section_html {
     # this only works if the section is the last item in the main block.
     my $has_footer = $page->wiki_opt('enable.last_section_footer') &&
                      $page->{wikifier}{main_block}{content}[-1] == $block;
-    if ($has_footer) {
-        $page->{wikifier}{main_block}{element}->configure(no_close_tag => 1);
-        $el->configure(no_close_tag => 1);
-    }
-    
+    $el->configure(no_close_tag => 1);
+
     # determine the page title if necessary.
     my $title    = $block->{name};
        $title    = $page->get('page.title') if $is_intro && !length $title;
