24f1550e5c90c0f8ec0e69b5e915e47d17f15574
Mitchell Cooper <githubspam@rlygd.net>
2014-09-14 15:25:18 -0400

    added invisible block

--- interfaces/PHP/Wikirequest.php
+++ interfaces/PHP/Wikirequest.php
@@ -1,2 +1,8 @@
+<?php
+
 // this file will provide an interface to access resources
 // this file will be placed within a web root or symlinked
+
+require_once('Wikiclient.php');
+
+?>
---
+++ lib/Wikifier/Block/Invisible.pm
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2014, Mitchell Cooper
+#
+# anything inside this block will not be displayed
+#
+package Wikifier::Block::Invisible;
+
+use warnings;
+use strict;
+
+use Scalar::Util 'blessed';
+
+our %block_types = (
+    invisible => {
+        parse => \&parse_invisible
+        html  => \&html_invisible
+    }
+);
+
+sub parse_invisible {
+    my ($block, $page) = @_;
+    foreach my $item (@{ $block->{content} }) {
+        next unless blessed $item;
+        $item->parse($page);
+    }
+}
+
+sub html_invisible {
+    my ($block, $page) = @_;
+    foreach my $item (@{ $block->{content} }) {
+        next unless blessed $item;
+        $item->html($page);
+    }
+}
+
+__PACKAGE__
