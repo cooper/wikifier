36bbdec6e4cb2b5f2497fc780d4c4487d488fc30
Mitchell Cooper <githubspam@rlygd.net>
2015-04-26 19:51:32 -0400

    bad session ID

--- lib/Wikifier/Server/Handlers.pm
+++ lib/Wikifier/Server/Handlers.pm
@@ -75,7 +75,8 @@ sub handle_resume {
 
     # session is too old or never existed.
     if (!$Wikifier::Server::sessions{ $msg->{session_id} }) {
-        $connection->send('resume' => { login_again => 1 });
+        Wikifier::l("Bad session ID for $$connection{id}; refusing authentication");
+        $connection->send(resume => { login_again => 1 });
         return;
     }
     
