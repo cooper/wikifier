67b4fe86e9f396b4614fb8e85ec6eac2580582cc
Mitchell Cooper <githubspam@rlygd.net>
2017-01-30 19:51:47 -0500

    in the process of adding infosec{}

--- lib/Wikifier/Block/Infobox.pm
+++ lib/Wikifier/Block/Infobox.pm
@@ -8,36 +8,45 @@ package Wikifier::Block::Infobox;
 use warnings;
 use strict;
 
-use Scalar::Util 'blessed';
-use HTML::Entities 'encode_entities';
+use Scalar::Util qw(blessed);
+use HTML::Entities qw(encode_entities);
+use Wikifier::Utilities qw(hash_maybe);
 
-our %block_types = (infobox => {
-    base  => 'map',
-    parse => \&infobox_parse,
-    html  => \&infobox_html
-});
+our %block_types = (
+    infobox => {
+        base  => 'map',
+        parse => \&infobox_parse,
+        html  => \&infobox_html
+    },
+    infosec => {
+        base  => 'map',
+        parse => \&infosec_parse,
+        html  => \&infosec_html,
+        invis => 1
+    }
+);
 
 sub infobox_parse {
-    my ($block, $page) = (shift, @_);
-    $block->parse_base(@_); # call hash parse.
+    my ($infobox, $page) = (shift, @_);
+    $infobox->parse_base(@_); # call hash parse.
 
     # search for image{}.
     # apply default width.
-    foreach my $item ($block->content_visible) {
+    foreach my $item ($infobox->content_visible) {
         next unless blessed $item && $item->{type} eq 'image';
         $item->{default_width} = '270px';
     }
 }
 
 sub infobox_html {
-    my ($block, $page, $el) = (shift, @_);
-    $block->html_base($page); # call hash html.
+    my ($infobox, $page, $el) = @_;
+    $infobox->html_base($page); # call hash html.
 
     # display the title if it exists.
-    if (length $block->{name}) {
+    if (length $infobox->{name}) {
         $el->create_child(
             class   => 'infobox-title',
-            content => $page->parse_formatted_text($block->{name})
+            content => $page->parse_formatted_text($infobox->{name})
         );
     }
 
@@ -48,42 +57,65 @@ sub infobox_html {
     );
 
     # append each pair.
-    foreach my $pair (@{ $block->{map_array} }) {
+    foreach my $pair (@{ $infobox->{map_array} }) {
         my ($key_title, $value, $key, $is_block) = @$pair;
-
-        # create the row.
-        my $tr = $table->create_child(
-            type  => 'tr',
-            class => 'infobox-pair'
-        );
-
-        # append table row with key.
-        if (length $key_title) {
-            $key_title = $page->parse_formatted_text($key_title);
-            $tr->create_child(
-                type       => 'td',
-                class      => 'infobox-key',
-                content    => $key_title
-            );
-            $tr->create_child(
-                type       => 'td',
-                class      => 'infobox-value',
-                content    => $value
-            );
-        }
-
-        # append table row without key.
-        else {
-            my $td = $tr->create_child(
-                type       => 'td',
-                class      => 'infobox-anon',
-                attributes => { colspan => 2 },
-                content    => $value
-            );
-            $td->add_class('infobox-text') if !$is_block;
-        }
-
+        infobox_add_row($infobox, $table, $key_title, $value, {
+            is_block => $is_block
+        });
     } # pair
+}
+
+sub infobox_add_row {
+    my ($infobox, $table, $key_title, $value, $opts_) = @_;
+    my %opts = hash_maybe $opts_;
+
+    # create the row.
+    my $tr = $table->create_child(
+        type  => 'tr',
+        class => 'infobox-pair',
+        hash_maybe $opts{tr_opts}
+    );
+
+    # append table row with key.
+    if (length $key_title) {
+        $key_title = $page->parse_formatted_text($key_title);
+        $tr->create_child(
+            type       => 'td',
+            class      => 'infobox-key',
+            content    => $key_title,
+            hash_maybe $opts{key_opts}
+        );
+        $tr->create_child(
+            type       => 'td',
+            class      => 'infobox-value',
+            content    => $value,
+            hash_maybe $opts{value_opts}
+        );
+    }
+
+    # append table row without key.
+    else {
+        my $td = $tr->create_child(
+            type       => 'td',
+            class      => 'infobox-anon',
+            attributes => { colspan => 2 },
+            content    => $value,
+            hash_maybe $opts{anon_opts}
+        );
+        $td->add_class('infobox-text') if !$opts{is_block};
+    }
+}
+
+sub infosec_parse {
+    my ($infosec, $page) = (shift, @_);
+    $infosec->parse_base(@_); # call hash parse.
+    if ($infosec->parent->type ne 'infobox') {
+        $infosec->warning('infosec{} outside of infobox{} does nothing');
+    }
+}
+
+sub infosec_html {
+
 }
 
 __PACKAGE__
