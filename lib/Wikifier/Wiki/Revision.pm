f1c519e79181496a4e251870dbfd2f2e98a55df3
Mitchell Cooper <githubspam@rlygd.net>
2017-05-07 02:04:00 -0400

    -l

--- lib/Wikifier/Wiki/Revision.pm
+++ lib/Wikifier/Wiki/Revision.pm
@@ -16,7 +16,7 @@ sub write_page {
     Lindent "WRITE($$page{name})";
 
     # do not allow symbolic link
-    if (-s $page->rel_path) {
+    if (-l $page->rel_path) {
         back;
         return 'Page is symbolically linked';
     }
@@ -59,7 +59,7 @@ sub delete_page {
 
     
     # do not allow symbolic link
-    if (-s $page->rel_path) {
+    if (-l $page->rel_path) {
         back;
         return 'Page is symbolically linked';
     }
@@ -84,7 +84,7 @@ sub move_page {
     Lindent "MOVE($$page{name} -> $new_name)";
     
     # this should never happen
-    if (-s $page->rel_path) {
+    if (-l $page->rel_path) {
         back;
         return 'Page is symbolically linked';
     }
