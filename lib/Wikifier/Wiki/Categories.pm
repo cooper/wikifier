d17875c2a5913190264518f454f8b6e028c6b1a6
Mitchell Cooper <githubspam@rlygd.net>
2018-11-12 19:24:22 -0800

    er got this backward

--- lib/Wikifier/Wiki/Categories.pm
+++ lib/Wikifier/Wiki/Categories.pm
@@ -128,8 +128,8 @@ sub display_cat_posts {
         # store time.
         # if this is the main page of the category, it should come first.
         my $main = $wiki->_is_main_page($cat_name_ne, $res);
-        $time = -$time if $main == 1;
-        $time = -'inf' if $main == 2;
+        $time += time  if $main == 1;
+        $time += 'inf' if $main == 2;
 
         # store res.
         $reses{$page_name} = $res;
